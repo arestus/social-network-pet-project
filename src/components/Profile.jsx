76a9f52217f86c458fae70a30014ec9ad6eb271c
arestus <arestus@gmail.com>
2021-06-22 21:56:17 +0300

    First commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          width="100%"
+          src="http://www.imgworlds.com/wp-content/uploads/2015/12/18-CONTACTUS-HEADER.jpg"
+          alt="funny cat with pult"
+        />
+      </div>
+      <div>
+        ava + description
+        <img src="" />
+      </div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
